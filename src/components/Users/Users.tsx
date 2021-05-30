a391f27b33482d8aec3c13c7871ac60bbbcd80c2
sf-minsk <sf-minsk@outlook.com>
2021-05-31 00:45:03 +0300

    axios

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,51 +2,25 @@ import React from "react";
 import styles from './users.module.css'
 import {v1} from "uuid";
 import {UsersPropsType} from "./UsersContainer";
+import axios from "axios";
+import userPhoto from '../../assets/images/userWhithoutAvatar.png'
 
 export const Users = (props: UsersPropsType) => {
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: v1(),
-                    photoUrl: 'https://iconarchive.com/download/i96376/iconsmind/outline/Internet.ico',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: v1(),
-                    photoUrl: 'https://iconarchive.com/download/i96376/iconsmind/outline/Internet.ico',
-                    followed: false,
-                    fullName: 'Alex',
-                    status: 'I am a boss too',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: v1(),
-                    photoUrl: 'https://iconarchive.com/download/i96376/iconsmind/outline/Internet.ico',
-                    followed: true,
-                    fullName: 'Andrey',
-                    status: 'I am a boss too',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                },
-                {
-                    id: v1(),
-                    photoUrl: 'https://iconarchive.com/download/i96376/iconsmind/outline/Internet.ico',
-                    followed: true,
-                    fullName: 'Olga',
-                    status: 'I am a boss too',
-                    location: {city: 'Brest', country: 'Belarus'}
-                },
-            ]
-        )
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            debugger
+            props.setUsers(response.data.items)
+        });
+
     }
+
     return <div>
         {
             props.users.map((u: any) => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed ? <button onClick={() => {
@@ -63,12 +37,12 @@ export const Users = (props: UsersPropsType) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
